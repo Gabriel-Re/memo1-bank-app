fb69eef92e5dfb56c8a41dd63fef8ebac86acc06
GabrielReFintech <gabriel.re@actionfintech.com>
2022-11-05 23:42:57 -0300

    feat: Creo esqueleto del servicio de transacciones

--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -6,8 +6,8 @@ import javax.persistence.*;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long TransactionId;
+    @GeneratedValue(strategy = GenerationType.AUTO)//Que se genere automaticamente de manera secuencial
+    private Long TransactionId; //Es un id unico, por eso el @id
 
     private long ClientCbu;
 
---
+++ src/main/java/com/aninfo/service/TransactionService.java
@@ -0,0 +1,15 @@
+package com.aninfo.service;
+
+import com.aninfo.model.Transaction;
+import com.aninfo.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    //@Autowired
+    //private AccountRepository accountRepository;
+    //Deberia guardarme un array o coleccion de transacciones?
+
+}
